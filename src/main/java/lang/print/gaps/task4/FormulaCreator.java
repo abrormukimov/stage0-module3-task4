04ef7fac1f05afb1404c5008d0806b759b681dcd
abrormukimov <abrormm34@gmail.com>
2022-09-25 00:01:09 +0500

    Update variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        byte a = 10;
-        byte b = 2;
-        double result = ((9 * Math.sqrt(a) + 2 + a - 7) * ((float)(a + b - 4 * a * b) / 2));
+        double a = 1000;
+        int b = 10;
+
+        System.out.println(
+                (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)
+        );
     }
 }
