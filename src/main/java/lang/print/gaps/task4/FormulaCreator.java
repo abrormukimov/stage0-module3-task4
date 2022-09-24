79914362212ad4174a103de45b96100114d6ce72
abrormukimov <abrormm34@gmail.com>
2022-09-24 20:19:03 +0500

    Update variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 10;
+        byte b = 2;
+        double result = ((9 * Math.sqrt(a) + 2 + a - 7) * ((float)(a + b - 4 * a * b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 10000;
+        int h = 100000;
+        int a = 90342;
+        int r = 9034212;
     }
 }
